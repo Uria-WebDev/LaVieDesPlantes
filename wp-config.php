0c81e0ec2098ec195ca23d5c0fedac5eab572232
Uria-WebDev <138232913+Uria-WebDev@users.noreply.github.com>
2025-02-03 15:25:56 +0100

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -24,10 +24,10 @@
 define( 'DB_NAME', 'La_vie_des_plantes' );
 
 /** Utilisateur de la base de données MySQL. */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'adm01' );
 
 /** Mot de passe de la base de données MySQL. */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'ykpw9L8n44LU5S' );
 
 /** Adresse de l’hébergement MySQL. */
 define( 'DB_HOST', 'localhost' );
